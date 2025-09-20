35dec294474efe5e7c068720fe1a3d05eab6983c
Evgeniy Kurenkov <zh.kurenkov@yandex.ru>
2025-09-20 14:14:08 +0400

    new files

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+  import Map from './components/map/Map.vue';
+  import Menu from './components/menu/Menu.vue';
+
+  export default {
+    data() {
+      return {
+        burgerIsOpen: false
+      }
+    },
+    methods: {
+      toggleBurgerMenu() {
+        this.burgerIsOpen = !this.burgerIsOpen
+        console.log(this.burgerIsOpen)
+      }
+    },
+    components: {Map, Menu}
+  }
+</script>
+  
+<template>
+  <Map />
+  <Menu :toggleBurgerMenu="toggleBurgerMenu" :burgerIsOpen="burgerIsOpen"/>
+</template>
+
+<style scoped>
+
+</style>
